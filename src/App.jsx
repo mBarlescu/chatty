b40a44bdc760d18e58784df22bf2e91345f46c36
Mark Barlescu <barlescu92@gmail.com>
2018-11-29 00:38:52 +0000

    able to receive data from server, about to attempt and turn that data into state and displayed on screen.

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,8 @@ class App extends Component {
 
   this.state = {
   currentUser: {name: "Bob"},
-  messages: [
-    {
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-      id:1
-    },
-    {
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-      id:2
-    }
-  ],
-}
+  messages: []
+};
   this.onNewPost = this.onNewPost.bind(this);
 }
 
